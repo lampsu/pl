7d1543e1ad637afa755f91fcd53de7ad69143eda
lampsu <102991235+lampsu@users.noreply.github.com>
2026-02-23 19:10:05 +0200

    Refactor Kinobase plugin initialization and button creation

--- kb.js
+++ kb.js
@@ -1,34 +1,41 @@
 (function () {
     'use strict';
 
-    // Функция отрисовки кнопки
-    function inject() {
-        var container = $('.full-start__buttons, .full-buttons');
-        if (container.length && !container.find('.view--kinobase').length) {
-            var btn = $('<div class="full-start__button selector view--kinobase" style="background: #ff5722 !important; color: #fff !important; margin-right: 10px;"><span>Kinobase</span></div>');
-            
-            btn.on('hover:enter', function () {
-                var movie = Lampa.Activity.active().card;
-                var url = movie.kinopoisk_id ? 'https://kinobase.org' + movie.kinopoisk_id : 'https://kinobase.org' + encodeURIComponent(movie.title || movie.name);
-                window.open(url, '_blank');
-            });
+    function initKinobase() {
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type == 'complite') {
+                var btn = $(`
+                    <div class="full-start__button selector view--kinobase" style="background: #fb5121 !important; color: #fff !important;">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org" style="margin-right: 8px; vertical-align: bottom;">
+                            <path d="M10 16.5V7.5L16 12L10 16.5Z" fill="white"/>
+                            <circle cx="12" cy="12" r="9" stroke="white" stroke-width="2"/>
+                        </svg>
+                        <span>Kinobase</span>
+                    </div>
+                `);
 
-            container.prepend(btn);
-            Lampa.Controller.enable('full');
-        }
+                btn.on('hover:enter', function () {
+                    var m = e.data.movie;
+                    var url = m.kinopoisk_id ? 'https://kinobase.org' + m.kinopoisk_id : 'https://kinobase.org' + encodeURIComponent(m.title || m.name);
+                    window.open(url, '_blank');
+                });
+
+                // Ждем отрисовки интерфейса и вставляем
+                setTimeout(function(){
+                    var container = e.container.find('.full-start__buttons');
+                    if (container.length && !container.find('.view--kinobase').length) {
+                        container.prepend(btn);
+                        Lampa.Controller.enable('full');
+                    }
+                }, 200);
+            }
+        });
     }
 
-    // Запуск через глобальный таймер (обход всех защит)
-    setInterval(function () {
-        if (window.location.hash.indexOf('full') > -1) {
-            inject();
-        }
-    }, 1000);
-
-    // Вывод в лог (проверьте в Настройки -> Инфо -> Лог)
-    console.log('Kinobase Plugin: Инициализация выполнена');
-    setTimeout(function() {
-        if (window.Lampa) Lampa.Noty.show('Kinobase готов');
-    }, 3000);
-
+    // Безопасный запуск
+    try {
+        initKinobase();
+    } catch (e) {
+        console.error('Kinobase error:', e);
+    }
 })();
