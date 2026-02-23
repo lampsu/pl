05b12a95aa5b3f96b94d9cd9345acc0ec8d3429f
lampsu <102991235+lampsu@users.noreply.github.com>
2026-02-23 19:04:09 +0200

    Refactor Kinobase plugin for improved functionality

--- kb.js
+++ kb.js
@@ -1,62 +1,62 @@
 (function () {
     'use strict';
 
-    // 1. Определение компонента поиска
-    function KinobaseComponent(object) {
-        this.create = function () {
-            var movie = object.movie;
-            var title = movie.title || movie.name;
-            // Прямой переход на поиск Kinobase
-            var url = 'https://kinobase.org' + encodeURIComponent(title);
-            window.open(url, '_blank');
-            return null;
+    function kinobase(object) {
+        var network = new Lampa.Reguest();
+        var scroll  = Lampa.Template.get('scroll');
+        var items   = [];
+        
+        // Поиск фильма на сайте Kinobase по названию и году
+        this.search = function (data) {
+            var url = 'https://kinobase.org' + encodeURIComponent(data.title);
+            
+            network.silent(url, function (html) {
+                // Здесь логика парсинга страницы поиска (упрощенно)
+                // Kinobase часто требует обхода CORS, поэтому рекомендуется использовать прокси
+                var link = 'https://kinobase.org' + data.kp_id; // Пример прямой ссылки через ID
+                
+                if (link) {
+                    var video = {
+                        url: link,
+                        title: data.title
+                    };
+                    Lampa.Player.play(video);
+                    Lampa.Player.playlist([video]);
+                } else {
+                    Lampa.Noty.show('Файл не найден на Kinobase');
+                }
+            });
         };
     }
 
+    // Интеграция кнопки в карточку фильма
     function startPlugin() {
-        Lampa.Component.add('kinobase', KinobaseComponent);
+        window.kinobase_plugin = true;
 
-        // 2. Слушатель отрисовки карточки
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                render(e);
+                var btn = $('<div class="full-start__button selector view--kinobase">' +
+                    '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org"><path d="M8 5v14l11-7z"/><path d="M0 0h24v24H0z" fill="none"/></svg>' +
+                    '<span>Kinobase</span>' +
+                    '</div>');
+
+                btn.on('hover:enter', function () {
+                    var movie = e.data.movie;
+                    var search = new kinobase();
+                    search.search({
+                        title: movie.title || movie.name,
+                        year: movie.release_date ? movie.release_date.split('-')[0] : '',
+                        kp_id: movie.kinopoisk_id
+                    });
+                });
+
+                e.container.find('.full-start__buttons').append(btn);
             }
         });
-
-        function render(e) {
-            // Ищем контейнер кнопок. В lampa.mx это .full-start__buttons
-            var container = e.render.find('.full-start__buttons');
-            
-            // Если кнопка уже есть, выходим
-            if (container.find('.button--kinobase').length) return;
-
-            // Создаем кнопку в стиле Lampa
-            var button = $(`
-                <div class="full-start__button button--secondary button--kinobase">
-                    <svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
-                        <path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"/>
-                    </svg>
-                    <span>Kinobase</span>
-                </div>
-            `);
-
-            button.on('click', function () {
-                Lampa.Component.item('kinobase', {
-                    movie: e.data.movie
-                });
-            });
-
-            // Вставляем кнопку после кнопки "Трейлер" или в конец
-            container.append(button);
-        }
     }
 
-    // Ожидание готовности Lampa
-    var wait = setInterval(function() {
-        if (typeof Lampa !== 'undefined' && Lampa.Listener) {
-            clearInterval(wait);
-            startPlugin();
-        }
-    }, 100);
-
+    if (window.appready) startPlugin();
+    else Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') startPlugin();
+    });
 })();
