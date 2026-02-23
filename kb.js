ee66985f7d6d28e19aff47b669e78d1a8019b7c2
lampsu <102991235+lampsu@users.noreply.github.com>
2026-02-23 19:02:43 +0200

    Update kb.js

--- kb.js
+++ kb.js
@@ -1,51 +1,62 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
-        console.log('Kinobase: Plugin initialization start');
-
-        // Регистрация компонента
-        Lampa.Component.add('kinobase_plugin', function() {
-            this.create = function() { return null; };
-            this.open = function(data) {
-                Lampa.Noty.show('Поиск на Kinobase: ' + data.title);
-                window.open('https://kinobase.org' + encodeURIComponent(data.title), '_blank');
-            };
-        });
-
-        // Функция вставки кнопки
-        function injectButton() {
-            // Ищем контейнеры кнопок в карточке
-            var footer = $('.full-start__buttons');
-            
-            if (footer.length > 0 && !$('.plugin--kinobase').length) {
-                console.log('Kinobase: Found container, injecting button...');
-                
-                var btn = $(`<div class="full-start__button button--secondary plugin--kinobase" style="background: #e67e22 !important; color: #fff !important;">
-                    <span>Kinobase</span>
-                </div>`);
-
-                btn.on('click', function () {
-                    var movie = Lampa.Activity.active().card;
-                    Lampa.Component.item('kinobase_plugin', {
-                        title: movie.title || movie.name
-                    });
-                });
-
-                footer.append(btn);
-            }
-        }
-
-        // Таймер для проверки DOM каждые 500мс (самый надежный метод для ПК)
-        setInterval(injectButton, 500);
+    // 1. Определение компонента поиска
+    function KinobaseComponent(object) {
+        this.create = function () {
+            var movie = object.movie;
+            var title = movie.title || movie.name;
+            // Прямой переход на поиск Kinobase
+            var url = 'https://kinobase.org' + encodeURIComponent(title);
+            window.open(url, '_blank');
+            return null;
+        };
     }
 
-    // Ожидание полной загрузки движка Lampa
-    var waitLampa = setInterval(function() {
-        if (typeof Lampa !== 'undefined' && Lampa.Component) {
-            clearInterval(waitLampa);
+    function startPlugin() {
+        Lampa.Component.add('kinobase', KinobaseComponent);
+
+        // 2. Слушатель отрисовки карточки
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type == 'complite') {
+                render(e);
+            }
+        });
+
+        function render(e) {
+            // Ищем контейнер кнопок. В lampa.mx это .full-start__buttons
+            var container = e.render.find('.full-start__buttons');
+            
+            // Если кнопка уже есть, выходим
+            if (container.find('.button--kinobase').length) return;
+
+            // Создаем кнопку в стиле Lampa
+            var button = $(`
+                <div class="full-start__button button--secondary button--kinobase">
+                    <svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
+                        <path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"/>
+                    </svg>
+                    <span>Kinobase</span>
+                </div>
+            `);
+
+            button.on('click', function () {
+                Lampa.Component.item('kinobase', {
+                    movie: e.data.movie
+                });
+            });
+
+            // Вставляем кнопку после кнопки "Трейлер" или в конец
+            container.append(button);
+        }
+    }
+
+    // Ожидание готовности Lampa
+    var wait = setInterval(function() {
+        if (typeof Lampa !== 'undefined' && Lampa.Listener) {
+            clearInterval(wait);
             startPlugin();
         }
-    }, 200);
+    }, 100);
 
 })();
